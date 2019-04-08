d02976dbb2b92e0275a9e5b9011105b3e88ce2ca
liudi-aditenmin <aDiTenMin0126>
2019-04-08 13:39:04 +0800

    修改教材申报扩展项目与图书名称大小

--- src/pages/home/teachingMaterial/navContent/newChooseBooks.vue
+++ src/pages/home/teachingMaterial/navContent/newChooseBooks.vue
@@ -721,7 +721,7 @@ export default {
         ],
         extensionName:[
           {required: true, message: "请填写名称", trigger: "blur" },
-          {min:1,max:100,message:'不能超过100个字符',trigger:'change,blur'}
+          {min:1,max:200,message:'不能超过200个字符',trigger:'change,blur'}
         ],
         noticeFiles:[
           {type:'array',required: false, message: "请至少上传一张图片", trigger: "blur" },
